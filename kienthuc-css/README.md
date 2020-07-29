55f80c1bdd16cfd05fc59e75142d3f12396ecb73
PhamMinhThanh <minhthanh95ptit@gmail.com>
2020-07-29 17:23:45 +0700

    Refactor code

--- kienthuc-css/README.md
+++
@@ -1,5 +0,0 @@
-# nodemy-basic
-
-#bai1-html
-#bai2,3,4-css
-#bai5-js co ban
--- kienthuc-js/setinterval.js
+++
@@ -1,9 +0,0 @@
-console.log(1);
-console.log(2);
-console.log('3');
-
-setInterval(function(){
-    console.log(new Date());
-},2000)
-
-console.log('4');
