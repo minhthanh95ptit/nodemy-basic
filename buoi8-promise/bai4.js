df9fd4bfcf2fe3c3ee447c5dd74950549807bb90
PhamMinhThanh <minhthanh95ptit@gmail.com>
2020-07-29 17:40:43 +0700

    Fix buoi8-promise bai4

--- buoi8-promise/bai4.js
+++
@@ -1,6 +0,0 @@
-var stops = [25, 55, 85, 100];
-$.each(stops, function(index, value){
-    setTimeout(function(){
-        $( ".progress-bar" ).css( "width", value + "%" ).attr( "aria-valuenow", value ); 
-    }, index * 1500);
-});
