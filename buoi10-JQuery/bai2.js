f0daa6c85de878e31d1892d941c208c1302c3a06
PhamMinhThanh <minhthanh95ptit@gmail.com>
2020-08-03 17:17:05 +0700

    Fix baitap buoi8,9

--- buoi10-JQuery/bai2.js
+++ buoi10-JQuery/bai2.js
@@ -1,6 +1,6 @@
 function result(){
-    var number1 = Number($('#input1').val());
-    var number2 = Number($('#input2').val());
+    var number1 = Number($('#number1').val());
+    var number2 = Number($('#number2').val());
 
     $('#kq').html(number1 + number2);
 }
